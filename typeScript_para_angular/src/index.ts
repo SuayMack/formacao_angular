7f8497fb218344e5b8fd39ef34f5758992db98df
SuayMack <pry_chu@hotmail.com>
2023-08-11 14:32:38 -0300

    CREATE - variables and objects

--- typeScript_para_angular/src/index.ts
+++ typeScript_para_angular/src/index.ts
@@ -1,12 +1,47 @@
-type heroi = {
-  name: string;
-  vulgo: string;
+// type heroi = {
+//   name: string;
+//   vulgo: string;
+// };
+
+// function printObjects(pessoa: heroi){
+//   console.log(pessoa);
+// }
+// printObjects({
+//   name: "Bruce Wayne",
+//   vulgo: "Batman"
+// })
+
+/* Variáveis */
+// primitivas
+let ligado: boolean = false;
+let nome: string = "Priscila";
+let idade: number = 43;
+let altura: number = 1.60;
+
+//tipos especiais: null, undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+//tipos abrangentes: any, void
+let retorno:void;
+let retornoView: any = false;
+
+//tipo objeto
+let produto: object = {
+  name: "Priscila",
+  cidade: "pr",
+  idade: 43
 };
 
-function printObjects(pessoa: heroi){
-  console.log(pessoa);
-}
-printObjects({
-  name: "Bruce Wayne",
-  vulgo: "Batman"
-})
+//tipo objeto tipado com previsibilidade
+type ProdutoLoja = {
+  nome: string;
+  preco: number;
+  unidades: number;
+};
+
+let meuProduto: ProdutoLoja = {
+  nome: "Tênis",
+  preco: 89.90,
+  unidades: 5
+};
