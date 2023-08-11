cfc2aee978fb48db6ddf7d000f8ce1d995aa7365
SuayMack <pry_chu@hotmail.com>
2023-08-11 15:55:34 -0300

    BUILD

--- typeScript_para_angular/build/index.js
+++ typeScript_para_angular/build/index.js
@@ -1,8 +1,42 @@
 "use strict";
-function printObjects(pessoa) {
-    console.log(pessoa);
-}
-printObjects({
-    name: "Bruce Wayne",
-    vulgo: "Batman"
-});
+// type heroi = {
+//   name: string;
+//   vulgo: string;
+// };
+// function printObjects(pessoa: heroi){
+//   console.log(pessoa);
+// }
+// printObjects({
+//   name: "Bruce Wayne",
+//   vulgo: "Batman"
+// })
+/* Variáveis */
+// primitivas
+let ligado = false;
+let nome = "Priscila";
+let idade = 43;
+let altura = 1.60;
+//tipos especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+//tipos abrangentes: any, void
+let retorno;
+let retornoView = false;
+//tipo objeto
+let produto = {
+    name: "Priscila",
+    cidade: "pr",
+    idade: 43
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.90,
+    unidades: 5
+};
+//arrays
+let dados = ["Priscila", "Wagner", "Davi", "Nick"];
+let dados2 = ["Priscila", "Wagner", "Davi", "Nick"];
+// arrays multi Types
+let infos = ["Priscila", 43, "Wagner"];
+// Tuplas **Tem que respeitar a ordem das informações
+let boleto = ["agua", 215.20, 6765446];
